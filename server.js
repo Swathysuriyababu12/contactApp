ac235a408f6da60a2870c2cf0f72de8fb9c767db
Swathy <100306927+Swathysuriyababu12@users.noreply.github.com>
2023-04-01 17:49:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ const authMiddleware = require("./middleware/auth")
 
 port = process.env.port
 
-app.use(express.static('./public'));
-app.use(express.json());
-app.use(cors());
-app.use('/' , authRoute)
-app.use('/' , authMiddleware,  contacts)
+// app.use(express.static('./public'));
+// app.use(express.json());
+// app.use(cors());
+// app.use('/' , authRoute)
+// app.use('/' , authMiddleware,  contacts)
 
 
-app.get('/' , authMiddleware, (req,res)=>{
+app.get('/', (req,res)=>{
     res.send('hello world')
 })
 
